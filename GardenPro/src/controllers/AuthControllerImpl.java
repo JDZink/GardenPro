3ebee52484c021fce7fefde1fa6e25c93d69d98e
JayZ <plainzink@gmail.com>
2017-03-13 10:28:31 -0600

    working on ping pong.

--- GardenPro/src/controllers/AuthControllerImpl.java
+++ GardenPro/src/controllers/AuthControllerImpl.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,12 @@ public class AuthControllerImpl implements AuthController {
 
 	@Autowired
 	AuthDAO dao;
-
+	
+	@GetMapping(path="ping")
+	public String ping(){
+		return "pong";
+	}
+	
 	@Override
 	@PostMapping(path = "/register")
 	public Map<String,String> register(@RequestBody String userJson) {
