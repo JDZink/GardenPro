83b9cea76343da3d20d7099da189f3467303fa75
Robert Helmick <roberthelmick@Roberts-MacBook-Pro.local>
2017-03-13 09:40:21 -0600

    created Planting entity

---
+++ GardenProJPA/src/entities/Planting.java
@@ -0,0 +1,24 @@
+package entities;
+
+import javax.persistence.*;
+
+public class Planting {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name="user_id")
+	private int userId;
+	
+	@Column(name="plant_id")
+	private int plantId;
+	
+	private int qty;
+	private int stage;
+	private int started;
+	private int planted;
+	private int harvest;
+	
+	
+	
+}
