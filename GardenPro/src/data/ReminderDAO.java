f5bec86512a240e0a96d381267e174fe94e04e44
Robert Helmick <roberthelmick@Roberts-MacBook-Pro.local>
2017-03-14 13:38:54 -0600

    created ReminderController, added fields to plant table and entity

--- GardenPro/src/data/ReminderDAO.java
+++ GardenPro/src/data/ReminderDAO.java
@@ -1,10 +1,20 @@
 package data;
 
-import java.util.Collection;
-
 import entities.Plant;
 import entities.Planting;
+import entities.Reminder;
+import entities.User;
 
 public interface ReminderDAO {
+	  public Reminder update(int id, Plant plant);
 
+	  public Reminder destroy(int id);
+	  
+	  public void reminderToWater(Planting p);
+	  
+	  public void reminderToSow(Planting p, User user);
+	  
+	  public void reminderToPlant(Planting p, User user);
+	  
+	  public void reminderToHarvest(Planting p, User user);
 }
--- GardenPro/src/data/ReminderDAOImpl.java
+++ GardenPro/src/data/ReminderDAOImpl.java
@@ -1,10 +1,19 @@
 package data;
 
+import java.time.LocalDate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import entities.User;
+import entities.Plant;
+import entities.Planting;
+import entities.Reminder;
 
 @Transactional
 @Repository
@@ -12,4 +21,58 @@ public class ReminderDAOImpl implements ReminderDAO{
 	@PersistenceContext
 	private EntityManager em;
 
+	@Override
+	public Reminder update(int id, Plant plant) {
+		Reminder r = em.find(Reminder.class,id);
+
+		em.flush();
+		return r;
+	}
+
+	@Override
+	public Reminder destroy(int id) {
+		Reminder r = em.find(Reminder.class, id);
+		em.remove(r);
+		return r;
+	}
+
+	@Override
+	public void reminderToWater(Planting p){
+		Reminder r = new Reminder();
+		r.setDate(p.getStarted().plusWeeks(2));
+		if(LocalDate.now() == r.getDate()){
+			//Trigger reminder
+		}
+	}
+	
+	@Override
+	public void reminderToSow(Planting p, User user){
+		Plant plant = em.find(Plant.class, p.getPlant());
+		Reminder r = new Reminder();
+
+		int sowDate = plant.getLastFrost() + plant.getEndGerm();
+		r.setDate(user.getFrostDate().minusWeeks(sowDate));
+		
+		if(LocalDate.now() == r.getDate()){
+			//Trigger reminder
+		}
+	}
+
+	@Override
+	public void reminderToPlant(Planting p, User user){
+		Plant plant = em.find(Plant.class, p.getPlant());
+		Reminder r = new Reminder();
+
+		int plantDate = plant.getLastFrost();
+		r.setDate(user.getFrostDate().minusWeeks(plant.getLastFrost()));
+		
+		if(LocalDate.now() == r.getDate()){
+			//Trigger reminder
+		}
+	}
+	
+	@Override
+	public void reminderToHarvest(Planting p, User user){
+		
+	}
 }
